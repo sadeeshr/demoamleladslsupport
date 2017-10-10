1c1bed43b2c95320b550b993a0f22187783e2236
sadeesh <sadeesh@telerain.com>
2017-10-10 21:56:26 +0800

    Initial Commit

---
+++ aws-config.js
@@ -0,0 +1,3 @@
+const aws_bots_config = '[{"name":"DslSupport","alias":"$LATEST","description":"DSL Support Bot.","commands-help":["I have an internet problem"],"region":"us-east-1"}]';
+const aws_cognito_identity_pool_id = 'us-east-1:8ac68463-d94c-4c7d-84c4-5cebee7bf95c';
+const aws_cognito_region = 'us-east-1';
